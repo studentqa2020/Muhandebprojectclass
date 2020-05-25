bac066e1372f2ca2a5602ae14ad202fb887195f5
muhan <muhan@DESKTOP-2E8DSGK.home>
2020-05-25 18:46:27 -0400

    at coding

---
+++ homework/src/main/java/com/generic/code/BaseLogin.java
@@ -0,0 +1,82 @@
+package com.generic.code;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.config.BaseConfig;
+import com.page.object.model.LoginPage;
+import com.util.Highlighter;
+import com.util.TakeAppScreenShot;
+import com.util.Wait;
+
+public class BaseLogin {
+
+	
+	protected static WebDriver driver;
+
+	public static void GetLogin() throws Throwable {
+
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+		Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		// open the browser
+		driver.get(BaseConfig.getconfig("URL"));
+		LoginPage Login = new LoginPage(driver);
+		System.out.println(driver.getTitle());
+
+		// click cookie
+		Login.getCookies();
+		Login.getCookies().click();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+
+		// click Sign_In
+		new Highlighter().getcolor(driver, Login.getSignBnt());
+		Login.getSignBnt().click();
+		
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+
+		// click Email
+		
+		Wait.getExplicitWaitClicable(driver, Login.getEmail());
+		new Highlighter().getcolor(driver, Login.getEmail(), "yellow");
+		Login.getEmail().sendKeys(BaseConfig.getconfig("Email"));
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+
+		// Click Password
+		new Highlighter().getcolor(driver, Login.getPassword(), "black");
+		Login.getPassword().sendKeys(BaseConfig.getconfig("Password"));
+		// Thread.sleep(3000);
+		Wait.getExplicitWaitClicable(driver, Login.getPassword());
+
+		// ScreenShort
+		TakeAppScreenShot.captureScreenShot(driver, "Login success");
+		System.out.println(driver.getTitle());
+
+		// Click Sign
+		new Highlighter().getcolor(driver, Login.getSubmited(), "green");
+		Login.getSubmited().click();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+
+	}
+
+	public static void main(String[] args) throws Throwable {
+		new BaseLogin().GetLogin();
+	}
+}
---
+++ homework/src/test/java/com/smoke/test/java/RunSmokeTest.java
@@ -0,0 +1,12 @@
+package com.smoke.test.java;
+
+import com.generic.code.SearchHome;
+
+public class RunSmokeTest {
+	
+	public static void main(String[] args) throws Throwable {
+		
+		SearchHome.getHomeprice();
+	}
+
+}
